d0fb38b5b0b27244efbb8b4683578e0eedadb12f
Alex <alex.hill@gmail.com>
2022-05-09 17:31:23 +0100

    build java-8 and java-11 images with different tags

--- build-image.sh
+++ build-image.sh
@@ -15,12 +15,24 @@ fi
 ORG=vimc
 NAME=openjdk-libsodium
 
-APP_DOCKER_TAG=${ORG}/${NAME}
-APP_DOCKER_COMMIT_TAG=${ORG}/${NAME}:${GIT_ID}
-APP_DOCKER_BRANCH_TAG=${ORG}/${NAME}:${GIT_BRANCH}
+APP_DOCKER_COMMIT_TAG=${ORG}/${NAME}:${GIT_ID}-8
+APP_DOCKER_BRANCH_TAG=${ORG}/${NAME}:${GIT_BRANCH}-8
 
 docker build \
     --pull \
+    --tag ${APP_DOCKER_BRANCH_TAG} \
+    --tag ${APP_DOCKER_COMMIT_TAG} \
+    .
+
+docker push ${APP_DOCKER_BRANCH_TAG}
+docker push ${APP_DOCKER_COMMIT_TAG}
+
+APP_DOCKER_COMMIT_TAG=${ORG}/${NAME}:${GIT_ID}-11
+APP_DOCKER_BRANCH_TAG=${ORG}/${NAME}:${GIT_BRANCH}-11
+
+docker build \
+    --pull \
+    --build-arg OPEN_JDK_BASE=openjdk:11 \
     --tag ${APP_DOCKER_BRANCH_TAG} \
     --tag ${APP_DOCKER_COMMIT_TAG} \
     .
